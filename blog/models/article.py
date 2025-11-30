3db8ec33411de1e1838ea871598742d9ca239783
Yanis Mikulskis <www.titan.ru95@mail.ru>
2025-11-30 23:39:27 +0300

    Подключил БД? добавил модель

---
+++ blog/models/article.py
@@ -0,0 +1,16 @@
+# from .database import db
+#
+#
+# class Article(db.Model):
+#     __tablename__ = 'Articles'
+#     id = db.Column(db.Integer, primary_key=True, autoincrement = True)
+#     header = db.Column(db.String(20), default = 'Not Header')
+#     text = db.Column(db.Text, default = 'Not text')
+#     created_at = db.Column(db.Datetime, default = datetime.utcnow)
+#     user_id = db.Column
+#
+#
+#     def __repr__(self):
+#         return
+
+
---
+++ blog/models/user.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, Boolean
+
+from .database import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    name = db.Column(db.String(30), default='Not Name')
+    is_staff = db.Column(db.Boolean, nullable=False, default=False)
+
+    def __repr__(self):
+        return f'<User #{self.id}:{self.username}'
+
+
+
